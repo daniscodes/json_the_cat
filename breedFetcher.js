a2f2f935e9ee744e712e89261626db3335d3aa5c
daniscodes <dan97@bell.net>
2022-08-04 21:15:54 -0400

    Refactored function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,12 @@
 const request = require('request');
-const myArgs = process.argv[2];
 
-const fetchBreed = function(breed, callback) {
+const fetchBreedDescription = function (breed, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
   request(url, (error, response, body) => {
-    // console.log(body[0]);
-    // console.log(typeof body);
-
     if (error) {
       callback(`Failed to request details: ${error}`, null);
     } else {
-
       const data = JSON.parse(body)[0];
-      // console.log(data[0]);
-      // const {name} = data;
-      // console.log(name)
       if (data) {
         callback(null, data.description);
       } else {
@@ -23,10 +15,5 @@ const fetchBreed = function(breed, callback) {
     }
   });
 };
-fetchBreed(myArgs, (error, data) => {
-  if (error) {
-    console.log('Error fetch details:', error);
-  } else {
-    console.log(data);
-  }
-});
+
+module.exports = { fetchBreedDescription };
