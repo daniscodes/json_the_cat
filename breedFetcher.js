3419387235a20c3538c280869b2c8e989b17b122
daniscodes <dan97@bell.net>
2022-08-04 20:46:32 -0400

    created fetchBreed function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,30 @@
 const request = require('request')
 
-const fetchBreed = function(breed, callback){
-  
+const fetchBreed = function (breed, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`
+  request(url, (error, response, body) => {
+    // console.log(body[0]);
+    // console.log(typeof body);
+
+
+    if (error) {
+      callback(`Failed to request details: ${error}`, null);
+    } else {
+
+      const data = JSON.parse(body)[0];
+      // console.log(data[0]);
+
+      // const {name} = data;
+      // console.log(name)
+      if (data) {
+        callback(null, data.description);
+      } else {
+        callback(`Failed to find breed ${breed}`, null)
+      }
+    }
+  })
 }
+fetchBreed(process.argv, (error, data) => {
+  console.log(error)
+  console.log(data)
+})
