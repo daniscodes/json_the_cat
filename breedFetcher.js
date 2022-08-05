d24a4f4b20df10c62df2c103fbaa1e7c72f8fb1a
daniscodes <dan97@bell.net>
2022-08-04 20:59:24 -0400

    Function works and linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,11 @@
-const request = require('request')
+const request = require('request');
+const myArgs = process.argv[2];
 
-const fetchBreed = function (breed, callback) {
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`
+const fetchBreed = function(breed, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
   request(url, (error, response, body) => {
     // console.log(body[0]);
     // console.log(typeof body);
-
 
     if (error) {
       callback(`Failed to request details: ${error}`, null);
@@ -13,18 +13,17 @@ const fetchBreed = function (breed, callback) {
 
       const data = JSON.parse(body)[0];
       // console.log(data[0]);
-
       // const {name} = data;
       // console.log(name)
       if (data) {
         callback(null, data.description);
       } else {
-        callback(`Failed to find breed ${breed}`, null)
+        callback(`Failed to find breed ${breed}`, null);
       }
     }
-  })
-}
-fetchBreed(process.argv, (error, data) => {
-  console.log(error)
-  console.log(data)
-})
+  });
+};
+fetchBreed(myArgs, (error, data) => {
+  //console.log(error);
+  console.log(data);
+});
